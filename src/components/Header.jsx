da5ed84b418b155dd3f819cfab1c547d726e804a
Nishan Ahmed <nishan@Nishans-MacBook-Air.local>
2024-07-08 11:54:05 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+
+function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="" />
+        <h1>Food Delivery App</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart (0)</Button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Meals.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import MealItem from "./MealItem";
+
+function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+      if (!response.ok) {
+        //
+      }
+
+      const meals = await response.json();
+      console.log(meals);
+      setLoadedMeals(meals);
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <>
+      <ul id="meals">
+        {loadedMeals.map((meal) => (
+          <MealItem key={meal.id} meal={meal}/>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Meals;
