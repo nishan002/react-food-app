8ad9663fd4f905ba24bee5b5e891261a19bf2b11
Nishan Ahmed <nishan@Nishans-MacBook-Air.local>
2024-07-16 11:25:23 +0600

    developing cart feature

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,18 +1,31 @@
 import React, { useContext } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
 
 function Cart() {
   const cartCtx = useContext(CartContext);
+  const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
+    console.log(totalPrice);
+    totalPrice + item.quantity + item * price;
+  }, 0);
 
   return (
     <Modal>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => {
-          <li key={item.id}>{item.name} - {item.quantity}</li>;
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>;
         })}
       </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly>Close</Button>
+        <Button>Go to Chekout</Button>
+      </p>
     </Modal>
   );
 }
