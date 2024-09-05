e8b646e96eeafab60aae44dc53c9fb959c9c90e8
Nishan Ahmed <nishan@Nishans-MacBook-Air.local>
2024-09-05 11:12:15 +0600

    sending post request

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -23,6 +23,19 @@ function Checkout() {
 
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries());
+
+    fetch('http://localhost:3000/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        order: {
+          item: cartCtx.items,
+          customer: customerData
+        }
+      })
+    })
   }
 
   return (
@@ -33,7 +46,7 @@ function Checkout() {
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-Mail Address" type="email" id="email" />
         <Input label="Stree" type="text" id="street" />
         <div className="control-row">
